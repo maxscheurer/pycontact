829e184feb13eef3558b64ce796d8ff47a49a34f
Peter Rodenkirch <p.rodenkirch@gmail.com>
2016-04-16 21:38:31 +0200

    Wurstgasgit log

---
+++ gui.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 520, 302))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout.addWidget(self.scrollArea, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 546, 22))
+        self.menubar.setDefaultUp(False)
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
+        self.menuFile.addAction(self.actionOpen)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.actionOpen.setText(_translate("MainWindow", "Open"))
+        self.actionOpen.setShortcut(_translate("MainWindow", "Ctrl+O"))
+
--- pycontact.py
+++ pycontact.py
@@ -10,178 +10,130 @@ from PyQt5.QtWidgets import (QWidget, QPushButton,
                              QFrame, QApplication, QSizePolicy)
 import shelve
 import numpy as np
+import gui
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+class MainWindow(QMainWindow, gui.Ui_MainWindow):
+	def __init__(self, parent=None):
+		super(MainWindow, self).__init__(parent)
+		self.setupUi(self)
 
-    def initUI(self):
+		self.setWindowTitle("pyContact")
 
-        centralWidget = QWidget()
+		self.painter = Canvas()
+		self.scrollArea.setWidget(self.painter)
 
-        menubar = QMenuBar()
-        menubar.addMenu('File')
+		self.actionOpen.triggered.connect(self.pushOpen)
 
-        grid = QGridLayout()
-        grid.setSpacing(1)
+	def pushOpen(self):
+		fnames = QFileDialog.getOpenFileNames(self, "Open file")
+		for file in fnames[0]:
+			self.file = file
+			break
+		lines = []
+		print(self.file)
+		with open(self.file, "r") as f:
+			for line in f.readlines():
+				lines.append(line)
+		self.makeContactFromLines(lines)
 
-        self.painter = Canvas()
-        grid.addWidget(self.painter, 0, 1)
-
-        self.heading = QLabel("Contacts")
-        grid.addWidget(self.heading, 0, 0)
-
-        mainLayout = QVBoxLayout(centralWidget)
-        mainLayout.addLayout(grid)
-        # mainLayout.addStretch()
-
-        # centralWidget.setLayout(grid)
-        self.setCentralWidget(centralWidget)
-
-        openFile = QAction(QIcon("open.png"), "Open", self)
-        openFile.setShortcut("Ctrl+O")
-        openFile.triggered.connect(self.showOpenFile)
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu("&File")
-        fileMenu.addAction(openFile)
-
-        # contact = Contact("A","B",1,2,[1,2])
-        # print(contact.type)
-        # print(contact.framenumber())
-
-        # User Defaults
-
-        # self.defaults = shelve.open("defaultState")
-        # if len(self.defaults) > 0:
-        #     self.userField.setText(self.defaults["user"])
-        #     self.user = self.defaults["user"]
-        #     self.hostField.setText(self.defaults["host"])
-        #     self.host = self.defaults["host"]
-        #     self.passwdField.setText(self.defaults["passwd"])
-        #     self.passwd = self.defaults["passwd"]
-        #     self.cbSaveLogin.toggle()
-
-        self.resize(1000, 550)
-        self.center()
-        self.setWindowTitle("pyContact")
-        self.show()
-
-    def showOpenFile(self):
-        fnames = QFileDialog.getOpenFileNames(self, "Open file")
-        for file in fnames[0]:
-            self.file = file
-            break
-        lines = []
-        print(self.file)
-        with open(self.file, "r") as f:
-            for line in f.readlines():
-                lines.append(line)
-        self.makeContactFromLines(lines)
-
-    def makeContactFromLines(self, lines):
-        self.contacts = []
-        for l in lines:
-            single_elements = l.split()
-            resA = single_elements[0]
-            residA = single_elements[1]
-            resB = single_elements[2]
-            residB = single_elements[3]
-            frame_info = np.array(single_elements[4:], dtype=float)
-            newContact = Contact(resA, residA, resB, residB, frame_info)
-            self.contacts.append(newContact)
-        print("new contacts: " + str(len(self.contacts)))
-        self.painter.contacts = self.contacts
-        self.painter.update()
-    # self.painter.take_screenshot("test")
-
-
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-    def pushLogin(self):
-
-        self.host = self.hostField.text()
-        self.user = self.userField.text()
-        self.passwd = self.passwdField.text()
-
-        try:
-            self.ssh = paramiko.SSHClient()
-            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.ssh.connect(self.host, username=self.user, password=self.passwd)
-            print("Successfully connected to " + self.host)
-
-        except paramiko.AuthenticationException:
-            print("Authentication error while connecting to " + host)
-
-        if self.cbSaveLogin.isChecked():
-            self.defaults = shelve.open("defaultState")
-            self.defaults["user"] = self.user
-            self.defaults["host"] = self.host
-            self.defaults["passwd"] = self.passwd
-            self.defaults.close()
+	def makeContactFromLines(self, lines):
+		self.contacts = []
+		for l in lines:
+			single_elements = l.split()
+			resA = single_elements[0]
+			residA = single_elements[1]
+			resB = single_elements[2]
+			residB = single_elements[3]
+			frame_info = np.array(single_elements[4:], dtype=float)
+			newContact = Contact(resA, residA, resB, residB, frame_info)
+			self.contacts.append(newContact)
+		print("new contacts: " + str(len(self.contacts)))
+		self.painter.contacts = self.contacts
+		self.painter.update()
 
 
 class Canvas(QWidget):
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-        self.initUI()
+		self.initUI()
 
-    def initUI(self):
+	def initUI(self):
 
-        # self.col = QColor(0, 0, 0)
+		# self.col = QColor(0, 0, 0)
+		# self.square = QFrame(self)
+		# self.square.setGeometry(0, 0, 1, 100)
+		# self.square.setStyleSheet("QWidget { background-color: %s }" %
+		# self.col.name())
+		self.contacts = 0
+		self.sizeX = 0
+		self.sizeY = 0
+		self.rendered = False
+		self.pixmap = 0
 
-        # self.square = QFrame(self)
-        # self.square.setGeometry(0, 0, 1, 100)
-        # self.square.setStyleSheet("QWidget { background-color: %s }" %
-        # self.col.name())
-        self.contacts = 0
+	def paintEvent(self, event):
 
-    def paintEvent(self, event):
+		qp = QPainter()
+		qp.begin(self)
 
-        qp = QPainter()
-        qp.begin(self)
-        self.drawContact(event, qp)
-        qp.end()
+		#render pixmap to resolve performance issues
+		if self.rendered:
+			self.drawRenderedContact(event, qp)
+		elif self.rendered == False and self.contacts:
+			self.renderContact()
+			self.rendered = True
 
-    def take_screenshot(self, filename):
-        print(filename)
-        p = QPixmap(self.size())
-        self.render(p)
-        p.save(filename, 'png', 100)
+		self.setMinimumSize(QSize(self.sizeX, self.sizeY))
 
-    def drawContact(self, event, qp):
+		qp.end()
 
-        startx = 80
-        orig_startx = startx
-        start_text = 10
-        textoffset = 13
-        rowheight = 22
-        offset = 10
-        color = QColor(0, 0, 0)
-        if self.contacts:
-            row = 0
-            for c in self.contacts:
-                # print total time of contact in nanoseconds
-                print(c.total_time(1, 0.5))
-                for x in c.scoreArray:
-                    qp.setPen(color)
-                    qp.setBrush(QColor(0, 200, 0, x * 50))
-                    # qp.setBrush(QColor(((1-x)*50), x*50,0))
-                    qp.drawRect(startx, row, offset, 20)
-                    startx += (offset)
-                    qp.setFont(QFont('Arial', 9))
-                    string = c.resA + c.residA + "-" + c.resB + c.residB
-                    qp.drawText(start_text, row+textoffset ,string)
-                startx = orig_startx
-                row += rowheight
+	def take_screenshot(self, filename):
+		print(filename)
+		p = QPixmap(self.size())
+		self.render(p)
+		p.save(filename, 'png', 100)
+
+	def renderContact(self):
+		startx = 80
+		orig_startx = startx
+		start_text = 10
+		textoffset = 13
+		rowheight = 22
+		offset = 10
+		blackColor = QColor(0, 0, 0)
+		whiteColor = QColor(255, 255, 255)
+
+		self.sizeX = (len(self.contacts[0].scoreArray) + startx) * offset
+		self.sizeY = len(self.contacts) * rowheight
+
+		self.pixmap = QPixmap(QSize(self.sizeX, self.sizeY))
+		p = QPainter()
+		p.begin(self.pixmap)
+
+		p.fillRect(0, 0, self.sizeX, self.sizeY, whiteColor)
+
+		row = 0
+		for c in self.contacts:
+			# print total time of contact in nanoseconds
+			#print(c.total_time(1, 0.5))
+			for x in c.scoreArray:
+				p.setPen(blackColor)
+				p.setBrush(QColor(0, 200, 0, x * 50))
+				# qp.setBrush(QColor(((1-x)*50), x*50,0))
+				p.drawRect(startx, row, offset, 20)
+				startx += (offset)
+				p.setFont(QFont('Arial', 9))
+				string = c.resA + c.residA + "-" + c.resB + c.residB
+				p.drawText(start_text, row+textoffset ,string)
+			startx = orig_startx
+			row += rowheight
+
+		p.end()
+		self.pixmap.save("test", 'png', 100)
+
+	def drawRenderedContact(self, event, qp):
+		qp.drawPixmap(0, 0, self.sizeX, self.sizeY, self.pixmap)
 
 
 class AnalysisView(QWidget):
@@ -221,8 +173,9 @@ def determine_ctype(resA, resB):
 
 def main():
     app = QApplication(sys.argv)
-    mainWindow = MainWindow()
-    sys.exit(app.exec_())
+    window = MainWindow()
+    window.show()
+    app.exec_()
 
 
 if __name__ == '__main__':
