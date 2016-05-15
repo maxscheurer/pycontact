aaddfcc073bddb3f6fbba24b940acb4b42de1d0d
maxscheurer <apfel.max@me.com>
2016-05-15 14:01:25 -0500

    test

---
+++ vis.tcl
@@ -0,0 +1,67 @@
+mol new /Users/mscheurer/Projects/pycontact/pycontact/rpn11_ubq_interface-ionized.psf 
+mol addfile /Users/mscheurer/Projects/pycontact/pycontact/short.dcd 
+mol delrep 0 top 
+mol representation NewCartoon 
+mol Color ColorID 3 
+mol selection {all} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 133andresname ASN and segname RN11) or (resid 9 and resname THR and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 88andresname GLN and segname RN11) or (resid 71 and resname LEU and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 85andresname ASP and segname RN11) or (resid 42 and resname ARG and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 48andresname GLU and segname RN11) or (resid 76 and resname GLY and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 121andresname VAL and segname RN11) or (resid 76 and resname GLY and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 125andresname THR and segname RN11) or (resid 74 and resname ARG and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 131andresname GLN and segname RN11) or (resid 35 and resname GLY and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 82andresname ALA and segname RN11) or (resid 73 and resname LEU and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 131andresname GLN and segname RN11) or (resid 34 and resname GLU and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 86andresname VAL and segname RN11) or (resid 49 and resname GLN and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 83andresname VAL and segname RN11) or (resid 73 and resname LEU and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 85andresname ASP and segname RN11) or (resid 72 and resname ARG and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 80andresname VAL and segname RN11) or (resid 74 and resname ARG and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 88andresname GLN and segname RN11) or (resid 70 and resname VAL and segname UBQ)} 
+mol addrep top 
+mol representation Licorice 
+mol Color Name 
+mol selection {(resid 50andresname MET and segname RN11) or (resid 75 and resname GLY and segname UBQ)} 
+mol addrep top 
