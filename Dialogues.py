d7c5a2e33676ab4c44deec6d7040a2a1bf730586
Max Scheurer <max.scheurer@me.com>
2016-11-26 23:13:16 +0100

    moving code around

---
+++ Dialogues.py
@@ -0,0 +1,147 @@
+from PyQt5.QtWidgets import QDialog, QGridLayout, QPushButton, QLabel,QLineEdit,QDialogButtonBox, QFileDialog, QCheckBox
+from PyQt5.QtCore import Qt
+from LoadConfiguration import Configuration
+class FileLoaderDialog(QDialog):
+    def __init__(self, parent = None):
+        super(FileLoaderDialog, self).__init__(parent)
+
+        self.psf = ""
+        self.dcd = ""
+
+        grid = QGridLayout(self)
+
+        buttonPsf = QPushButton("PSF")
+        buttonPsf.clicked.connect(self.pick_psf)
+
+        buttonDcd = QPushButton("DCD")
+        buttonDcd.clicked.connect(self.pick_dcd)
+
+        grid.addWidget(buttonPsf,0,0)
+        grid.addWidget(buttonDcd,0,1)
+
+        cutoffLabel = QLabel("distance cutoff: ")
+        cutoffAngleLabel = QLabel("angle cutoff: ")
+        cutoffHbondLabel = QLabel("acc-h cutoff: ")
+        selection1Label = QLabel("selection 1: ")
+        selection2Label = QLabel("selection 2: ")
+
+        self.cutoffField = QLineEdit("5.0")
+        self.cutoffAngleField = QLineEdit("120")
+        self.cutoffHbondField = QLineEdit("2.5")
+        self.selection1Field = QLineEdit("segid RN11")
+        self.selection2Field = QLineEdit("segid UBQ")
+
+        grid.addWidget(cutoffLabel,1,0)
+        grid.addWidget(cutoffAngleLabel,2,0)
+        grid.addWidget(cutoffHbondLabel,3,0)
+        grid.addWidget(selection1Label,4,0)
+        grid.addWidget(selection2Label,5,0)
+
+        grid.addWidget(self.cutoffField,1,1)
+        grid.addWidget(self.cutoffAngleField,2,1)
+        grid.addWidget(self.cutoffHbondField,3,1)
+        grid.addWidget(self.selection1Field,4,1)
+        grid.addWidget(self.selection2Field,5,1)
+
+
+        # OK and Cancel buttons
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
+            Qt.Horizontal, self)
+        buttons.accepted.connect(self.accept)
+        buttons.rejected.connect(self.reject)
+        grid.addWidget(buttons,6,0)
+
+    def pick_psf(self):
+        psfname = QFileDialog.getOpenFileNames(self, "Open psf")
+        for file in psfname[0]:
+            self.psf = file
+            break
+    def pick_dcd(self):
+        dcdname = QFileDialog.getOpenFileNames(self, "Open dcd")
+        for file in dcdname[0]:
+            self.dcd = file
+            break
+
+    def configuration(self):
+        config = Configuration(self.psf,self.dcd, float(self.cutoffField.text()),float(self.cutoffHbondField.text()),float(self.cutoffAngleField.text()),self.selection1Field.text(),self.selection2Field.text())
+        return config
+
+    # static method to create the dialog and return (date, time, accepted)
+    @staticmethod
+    def getConfig(parent = None):
+        dialog = FileLoaderDialog(parent)
+        result = dialog.exec_()
+        config = dialog.configuration()
+        return (config, result == QDialog.Accepted)
+
+
+class AnalysisDialog(QDialog):
+    def __init__(self, parent = None):
+        super(AnalysisDialog, self).__init__(parent)
+
+        grid = QGridLayout(self)
+
+        title1 = QLabel("selection 1")
+        title2 = QLabel("selection 2")
+        indexLabel = QLabel("index: ")
+        nameLabel = QLabel("atom name: ")
+        residLabel = QLabel("resid: ")
+        resnameLabel = QLabel("resname: ")
+        segidLabel = QLabel("segid: ")
+
+        self.index1Checkbox = QCheckBox()
+        self.name1Checkbox = QCheckBox()
+        self.resid1Checkbox = QCheckBox()
+        self.resname1Checkbox = QCheckBox()
+        self.segid1Checkbox = QCheckBox()
+
+        self.index2Checkbox = QCheckBox()
+        self.name2Checkbox = QCheckBox()
+        self.resid2Checkbox = QCheckBox()
+        self.resname2Checkbox = QCheckBox()
+        self.segid2Checkbox = QCheckBox()
+
+        grid.addWidget(title1,0,1)
+        grid.addWidget(title2,0,2)
+        grid.addWidget(indexLabel,1,0)
+        grid.addWidget(nameLabel,2,0)
+        grid.addWidget(residLabel,3,0)
+        grid.addWidget(resnameLabel,4,0)
+        grid.addWidget(segidLabel,5,0)
+
+        grid.addWidget(self.index1Checkbox,1,1)
+        grid.addWidget(self.name1Checkbox,2,1)
+        grid.addWidget(self.resid1Checkbox,3,1)
+        grid.addWidget(self.resname1Checkbox,4,1)
+        grid.addWidget(self.segid1Checkbox,5,1)
+
+        grid.addWidget(self.index2Checkbox,1,2)
+        grid.addWidget(self.name2Checkbox,2,2)
+        grid.addWidget(self.resid2Checkbox,3,2)
+        grid.addWidget(self.resname2Checkbox,4,2)
+        grid.addWidget(self.segid2Checkbox,5,2)
+
+        # OK and Cancel buttons
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
+            Qt.Horizontal, self)
+        buttons.accepted.connect(self.accept)
+        buttons.rejected.connect(self.reject)
+        grid.addWidget(buttons,6,0)
+
+
+    def mapping(self):
+        # atom types will not be supported in the future
+        map1 = [self.index1Checkbox.isChecked(),0,self.name1Checkbox.isChecked(),self.resid1Checkbox.isChecked(),self.resname1Checkbox.isChecked(),self.segid1Checkbox.isChecked()]
+        map2 = [self.index2Checkbox.isChecked(),0,self.name2Checkbox.isChecked(),self.resid2Checkbox.isChecked(),self.resname2Checkbox.isChecked(),self.segid2Checkbox.isChecked()]
+        print map1, map2
+        return [map1,map2]
+
+    # static method to create the dialog and return (date, time, accepted)
+    @staticmethod
+    def getMapping(parent = None):
+        dialog = AnalysisDialog(parent)
+        result = dialog.exec_()
+        mapping = dialog.mapping()
+        return (mapping, result == QDialog.Accepted)
---
+++ ExportTabWidget.py
@@ -0,0 +1,122 @@
+from PyQt5.QtWidgets import QTabWidget, QWidget, QGridLayout, QLabel, QPushButton, QComboBox, QLineEdit
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+from Plotters import *
+class ExportTabWidget(QTabWidget):
+
+    valueUpdated = pyqtSignal(str, str)
+
+    def __init__(self, parent = None):
+       super(ExportTabWidget, self).__init__(parent)
+
+       self.tab1 = QWidget()
+       self.tab2 = QWidget()
+
+       self.addTab(self.tab1, "View")
+       self.addTab(self.tab2, "Histogram")
+       self.tab1UI()
+       self.tab2UI()
+       self.setWindowTitle("Export")
+       self.contacts = []
+       self.threshold = 0
+       self.nsPerFrame = 0
+
+    def setThresholdAndNsPerFrame(self, currentThreshold, currentNsPerFrame):
+        self.threshold = currentThreshold
+        self.nsPerFrame = currentNsPerFrame
+
+    def tab1UI(self):
+        grid = QGridLayout()
+        self.tab1.setLayout(grid)
+
+        self.tab1.exportLabel = QLabel("Export current view: ")
+
+        self.tab1.saveButton = QPushButton("Export")
+        self.tab1.saveButton.setAutoDefault(False)
+        self.tab1.saveButton.clicked.connect(self.pushSave)
+
+        self.tab1.formatBox = QComboBox()
+        self.tab1.formatBox.addItem("PNG")
+        self.tab1.formatBox.addItem("SVG")
+
+        grid.addWidget(self.tab1.exportLabel, 0, 0)
+        grid.addWidget(self.tab1.saveButton, 0, 1)
+
+        grid.addWidget(self.tab1.formatBox, 2, 0)
+
+
+    def tab2UI(self):
+        self.grid1 = QGridLayout()
+        self.tab2.setLayout(self.grid1)
+
+        self.tab2.histPlot = HistPlotter(None, width=8, height=5, dpi=60)
+        self.grid1.addWidget(self.tab2.histPlot, 3, 0, 1, 4)
+
+        self.tab2.histTypeBox = QComboBox()
+        self.tab2.histTypeBox.addItem("General Histogram")
+        self.tab2.histTypeBox.addItem("Bin per Contact")
+
+        self.grid1.addWidget(self.tab2.histTypeBox, 0, 3)
+
+        self.tab2.attributeBox = QComboBox()
+        self.tab2.attributeBox.addItem("Mean Score")
+        self.tab2.attributeBox.addItem("Median Score")
+        self.tab2.attributeBox.addItem("Mean Lifetime")
+        self.tab2.attributeBox.addItem("Median Lifetime")
+        self.tab2.attributeBox.addItem("Hbond percentage")
+
+        self.grid1.addWidget(self.tab2.attributeBox, 1, 3)
+
+        self.tab2.plotButton = QPushButton("Show Preview")
+        self.tab2.plotButton.setAutoDefault(False)
+        self.tab2.plotButton.clicked.connect(self.pushPlot)
+        self.grid1.addWidget(self.tab2.plotButton, 0, 0, 1, 3)
+
+        self.tab2.saveButton = QPushButton("Save Histogram")
+        self.tab2.saveButton.setAutoDefault(False)
+        self.tab2.saveButton.clicked.connect(self.saveHist)
+        self.grid1.addWidget(self.tab2.saveButton, 1, 0, 1, 1)
+
+        self.tab2.formatLabel = QLabel("Format: ")
+        self.grid1.addWidget(self.tab2.formatLabel, 1, 1)
+
+        self.tab2.xTicksFontSizeLabel = QLabel("bin per contact font size: ")
+        self.grid1.addWidget(self.tab2.xTicksFontSizeLabel, 2, 0)
+
+        self.tab2.xTicksFontSizeField = QLineEdit("11")
+        self.grid1.addWidget(self.tab2.xTicksFontSizeField, 2, 1)
+
+        self.tab2.formatBox = QComboBox()
+        self.tab2.formatBox.addItem("pdf")
+        self.tab2.formatBox.addItem("png")
+        self.tab2.formatBox.addItem("svg")
+	self.tab2.formatBox.addItem("eps")
+        self.grid1.addWidget(self.tab2.formatBox, 1, 2)
+
+    def saveHist(self):
+        self.plotHist()
+
+        fileName = QFileDialog.getSaveFileName(self, 'Export Path')
+        if len(fileName[0]) > 0:
+            path, file_extension = os.path.splitext(fileName[0])
+            self.tab2.histPlot.saveFigure(path, self.tab2.formatBox.currentText())
+
+    def pushPlot(self):
+        self.plotHist()
+
+    def plotHist(self):
+        sip.delete(self.tab2.histPlot)
+        self.tab2.histPlot = HistPlotter(None, width=8, height=5, dpi=60)
+        self.grid1.addWidget(self.tab2.histPlot, 3, 0, 1, 4)
+        if self.tab2.histTypeBox.currentText() == "General Histogram":
+            self.tab2.histPlot.plotGeneralHist(self.contacts, self.tab2.attributeBox.currentText(), self.threshold, self.nsPerFrame)
+        elif self.tab2.histTypeBox.currentText() == "Bin per Contact":
+            self.tab2.histPlot.plotContactHist(self.contacts, self.tab2.attributeBox.currentText(), self.threshold, self.nsPerFrame,int(self.tab2.xTicksFontSizeField.text()))
+
+        self.tab2.histPlot.update()
+
+    def pushSave(self):
+        fileName = QFileDialog.getSaveFileName(self, 'Export Path')
+        self.valueUpdated.emit(fileName[0], self.tab1.formatBox.currentText())
+
+    def setContacts(self, currentContacts):
+        self.contacts = currentContacts
