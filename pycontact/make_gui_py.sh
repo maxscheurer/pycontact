127ec6c1227d98360063f16d8d0c7e69feeefa65
Max Scheurer <max.scheurer@me.com>
2016-12-21 21:36:40 +0100

    test

--- pycontact/make_gui_py.sh
+++
@@ -1,2 +0,0 @@
-pyuic5-2.7 gui.ui > gui.py
-pyuic5-2.7 settings.ui > settings.py
--- pycontact/shared/compile.sh
+++
@@ -1,2 +0,0 @@
-g++ -Wall -g -std=c++11 -fPIC -O -shared gridsearch.C -o libgridsearch.so
-#g++ -Wall -g -std=c++11 -fPIC -O -shared gridsearch.C -o libgridsearch.so -lpython2.7
