c7bad8e4e3584ca1aa16df5d0a03871b19c7c0b4
Max Scheurer <max.scheurer@me.com>
2017-01-22 12:12:42 +0100

    cleaner names in cymodules, gitignore now ignoring cython generated c and cpp code

--- cy_modules/setup.py
+++ cy_modules/setup.py
@@ -3,7 +3,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("cy_gridsearch",["gridsearch.pyx"],language="c++",
+    ext_modules = [Extension("cy_gridsearch",["cy_gridsearch.pyx"],language="c++",
     extra_compile_args=["-std=c++11"],
     extra_link_args=["-std=c++11"]),
 ])
