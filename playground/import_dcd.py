cfc03f7da8c0ce733e350683320f1879c17c97d9
maxscheurer <max.scheurer@me.com>
2017-01-22 18:36:48 +0100

    removing playground and testing files that were obsolete

--- playground/import_dcd.py
+++
@@ -1,8 +0,0 @@
-import MDAnalysis as md
-from MDAnalysis.lib.formats.libdcd import DCDFile
-
-psf = "../rpn11_ubq_interface-ionized.psf"
-dcd = "../short.dcd"
-
-# u = md.Universe(psf,dcd)
-dcdfile = DCDFile(dcd)
