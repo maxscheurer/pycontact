5d7b0637ac4ac2b8b201558752e64c0f55be1b55
Peter Rodenkirch <p.rodenkirch@gmail.com>
2017-04-04 19:28:45 +0200

    using appropriate parameters in cy_sasa_cuda.py (setup.py broken)

--- PyContact/cy_modules/src/sasaCuda.hh
+++ PyContact/cy_modules/src/sasaCuda.hh
@@ -1,1 +1,3 @@
-double calculate_sasa_cuda(double test);
+double calculate_sasa_cuda(const float *pos,int natoms, float pairdist, int allow_double_counting, int maxpairs,
+                           const float *radius,const int npts, double srad, int pointstyle, int restricted,
+                           const int* restrictedList);
