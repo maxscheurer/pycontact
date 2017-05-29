bfc6ee4f3d1b1ce12d5002b3e265f1a852265023
Max Scheurer <max.scheurer@me.com>
2017-05-29 13:55:16 +0200

    updated automation example

--- examples/automation.py
+++ examples/automation.py
@@ -1,40 +1,8 @@
-from PyContact.core.Scripting import PyContactJob
+from PyContact.core.Scripting import PyContactJob, JobConfig
 
-# jobAllMD = PyContactJob("/run/media/max/max_data/dodecin/MD_all/run1/dodecin_mg-ionized.psf",
-#                    "/run/media/max/max_data/dodecin/MD_all/md_all.dcd", "md_all", JobConfig(5.0, 2.5, 120, [0,0,0,0,0,1], [0,0,0,0,0,1], "resname LIG", "resname TIP3"))
-#
-# jobAllMD.runJob(8)
-# jobAllMD.writeSessionToFile()
-
-# jobAllMD_womg = PyContactJob("/run/media/max/max_data/dodecin/MD_all_woMG/run1/dodecin_womg-ionized.psf",
-#                    "/run/media/max/max_data/dodecin/MD_all_woMG/md_all_womg.dcd", "md_all_womg", JobConfig(5.0, 2.5, 120, [0,0,0,0,0,1], [0,0,0,0,0,1], "resname LIG", "resname TIP3"))
-#
-# jobAllMD_womg.runJob(8)
-# jobAllMD_womg.writeSessionToFile()
-
-# job_single = PyContactJob("/run/media/max/max_data/dodecin/MD_single_lmf/run1/dodecin_mg_single_lig-ionized.psf",
-#                    "/run/media/max/max_data/dodecin/MD_single_lmf/md_single_lmf.dcd", "md_single", JobConfig(5.0, 2.5, 120, [0,0,0,0,0,1], [0,0,0,0,0,1], "resname LMF", "resname TIP3"))
-#
-# job_single.runJob(8)
-# job_single.writeSessionToFile()
-#
-
-# jobAllMD_gln = PyContactJob("/run/media/max/max_data/dodecin/MD_all/run1/dodecin_mg-ionized.psf",
-#                    "/run/media/max/max_data/dodecin/MD_all/md_all.dcd", "md_all_gln", JobConfig(5.0, 2.5, 120, [0,0,0,0,0,1], [0,0,0,0,0,1], "resname LIG", "resid 55 and protein"))
-#
-# jobAllMD_gln.runJob(8)
-# jobAllMD_gln.writeSessionToFile()
-
-
-# jobAllMD_womg_gln = PyContactJob("/run/media/max/max_data/dodecin/MD_all_woMG/run1/dodecin_womg-ionized.psf",
-#                    "/run/media/max/max_data/dodecin/MD_all_woMG/md_all_womg.dcd", "md_all_womg_gln", JobConfig(5.0, 2.5, 120, [0,0,1,0,0,1], [0,0,0,0,1,0], "resname LIG", "resid 55 and protein"))
-#
-# jobAllMD_womg_gln.runJob(8)
-# jobAllMD_womg_gln.writeSessionToFile()
-#
-#
-# job_single_gln = PyContactJob("/run/media/max/max_data/dodecin/MD_single_lmf/run1/dodecin_mg_single_lig-ionized.psf",
-#                    "/run/media/max/max_data/dodecin/MD_single_lmf/md_single_lmf.dcd", "md_single_gln", JobConfig(5.0, 2.5, 120, [0,0,1,0,0,1], [0,0,0,0,1,0], "resname LMF", "resid 55 and protein"))
-#
-# job_single_gln.runJob(8)
-# job_single_gln.writeSessionToFile()
+# define input files and parameters
+job = PyContactJob("/path/to/topology", "/path/to/trajectory", "title", JobConfig(5.0, 2.5, 120, [0,0,1,1,0], [0,0,1,1,0], "segid A", "segid B"))
+# running the job on 4 cores
+job.runJob(4)
+# writing the session to file
+job.writeSessionToFile()
