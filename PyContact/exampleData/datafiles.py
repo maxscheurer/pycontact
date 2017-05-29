0886d16f812e00f2ca2fec0bdf28dcae653dd750
Max Scheurer <max.scheurer@me.com>
2017-05-29 18:51:58 +0200

    updating sample files

--- PyContact/exampleData/datafiles.py
+++ PyContact/exampleData/datafiles.py
@@ -2,8 +2,8 @@
 
 from pkg_resources import resource_filename as res
 
-DCD = res(__name__, './short.dcd')
-PSF = res(__name__, './rpn11_ubq_interface-ionized.psf')
+DCD = res(__name__, './rpn11_ubq.dcd')
+PSF = res(__name__, './rpn11_ubq.psf')
 XTC = res(__name__, './md_noPBC.xtc')
 TPR = res(__name__, './md.tpr')
 DEFAULTSESSION = res(__name__, './defaultsession')
