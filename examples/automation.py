4d10e5783d703a5994aade781843f5f09d204ef3
Peter Rodenkirch <p.rodenkirch@gmail.com>
2018-04-14 00:10:11 +0200

    numpy implementation for accumulating scores

--- examples/automation.py
+++ examples/automation.py
@@ -3,6 +3,6 @@ from PyContact.core.Scripting import PyContactJob, JobConfig
 # define input files and parameters
 job = PyContactJob("/path/to/topology", "/path/to/trajectory", "title", JobConfig(5.0, 2.5, 120, [0,0,1,1,0], [0,0,1,1,0], "segid A", "segid B"))
 # running the job on 4 cores
-job.runJob(4)
+job.runJob(2)
 # writing the session to file
 job.writeSessionToFile()
